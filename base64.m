198e22d2dcfc9656b950a34085e5e7ae7b9641c0
Dario Malchiodi <malchiodi@di.unimi.it>
2013-01-08 21:05:05 +0100

    Added license and copyright statements

--- base64.m
+++ base64.m
@@ -1,5 +1,22 @@
 (* ::Package:: *)
 
+(******************************************************************************
+       Copyright (C) 2010 Dario Malchiodi <malchiodi@di.unimi.it>
+
+This file is part of base64.
+base64 is free software; you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2.1 of the License, or (at your option)
+any later version.
+base64 is distributed in the hope that it will be useful, but without any
+warranty; without even the implied warranty of merchantability or fitness
+for a particular purpose. See the GNU Lesser General Public License for
+more details.
+You should have received a copy of the GNU Lesser General Public License
+along with base64; if not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************)
+
 (* : Title : base64 *)
 (* : Context : base64` *)
 (* : 
